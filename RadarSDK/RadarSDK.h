9c243cec17c0690012c4d5fc9b1f7892edb1bde2
cicimen <egemengulkilik@gmail.com>
2021-10-09 20:15:57 +0300

    init project

---
+++ RadarSDK/RadarSDK.h
@@ -0,0 +1,18 @@
+//
+//  RadarSDK.h
+//  RadarSDK
+//
+//  Created by Egemen Gulkilik on 9.10.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RadarSDK.
+FOUNDATION_EXPORT double RadarSDKVersionNumber;
+
+//! Project version string for RadarSDK.
+FOUNDATION_EXPORT const unsigned char RadarSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RadarSDK/PublicHeader.h>
+
+
